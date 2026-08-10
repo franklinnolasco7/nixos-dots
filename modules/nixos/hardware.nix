1a2880f237176549852b48f8e5d3e36a920cc3ff
Frank <116436582+franklinnolasco7@users.noreply.github.com>
2026-08-11 03:12:48 +0800

    refactor: some changes for fixes

--- modules/nixos/hardware.nix
+++ modules/nixos/hardware.nix
@@ -1,6 +1,19 @@
+{ config, ... }:
+
 {
   hardware.graphics.enable = true;
+
   services.power-profiles-daemon.enable = true;
-  boot.kernelModules = [ "acer-wmi-battery" ];
-  boot.kernel.sysctl = { "vm.swappiness" = 180; };
+
+  boot.extraModulePackages = with config.boot.kernelPackages; [
+    acer-wmi-battery
+  ];
+
+  boot.kernelModules = [
+    "acer-wmi-battery"
+  ];
+
+  boot.kernel.sysctl = {
+    "vm.swappiness" = 180;
+  };
 }
