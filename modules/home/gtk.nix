07f56d828263c2887a7e7f046e3043670bd8dc9f
Frank <116436582+franklinnolasco7@users.noreply.github.com>
2026-08-11 05:50:54 +0800

    refactor: add glib to make gtk work and update TODO

--- modules/home/gtk.nix
+++ modules/home/gtk.nix
@@ -2,10 +2,13 @@
 
 {
   gtk = {
+    enable = true;
+
     theme = {
       name = "Adwaita-dark";
       package = pkgs.gnome-themes-extra;
     };
+
     iconTheme = {
       name = "Papirus-Dark";
       package = pkgs.papirus-icon-theme;
