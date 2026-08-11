c2d14b1b544579351338057990c30c304090b2ef
Frank <116436582+franklinnolasco7@users.noreply.github.com>
2026-08-12 02:49:36 +0800

    fix: remove invalid virtualisation.libvirt option

--- modules/nixos/virtualization.nix
+++ modules/nixos/virtualization.nix
@@ -1,11 +1,7 @@
 { pkgs, ... }:
-
 {
   virtualisation.libvirtd.enable = true;
-  virtualisation.libvirt.qemu.enable = true;
-
   programs.virt-manager.enable = true;
-
   environment.systemPackages = with pkgs; [
     virt-viewer
     spice-gtk
