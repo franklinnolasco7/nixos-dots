322e769431b3bad5aa2f05b978e6a6a8d5efecd4
Frank <116436582+franklinnolasco7@users.noreply.github.com>
2026-08-11 09:27:55 +0800

    chore: update git config

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -6,6 +6,13 @@
       user = {
         name = "Frank";
         email = "116436582+franklinnolasco7@users.noreply.github.com";
+        signingkey = "/home/frank/.ssh/id_ed25519.pub";
+      };
+      gpg = {
+        format = "ssh";
+      };
+      commit = {
+        gpgsign = true;
       };
       init.defaultBranch = "main";
       fetch.prune = true;
@@ -13,4 +20,7 @@
       pull.rebase = false;
     };
   };
+
+  home.file.".config/git/allowed_signers".text =
+    "116436582+franklinnolasco7@users.noreply.github.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHtOXqQXYBzKOemkphICYNyUGXOBAMe2HH3bxszTd6R0";
 }
