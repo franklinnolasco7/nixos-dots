5e57517653525379e9e443dd6032d00c14ede210
Frank <116436582+franklinnolasco7@users.noreply.github.com>
2026-08-11 10:04:28 +0800

    chore(git): update git configuration and SSH signature settings

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -1,16 +1,32 @@
 { ... }:
-
 {
   programs.git = {
     enable = true;
-    userName = "Frank";
-    userEmail = "REDACTED";
-
     settings = {
+      user = {
+        name = "Frank";
+        email = "116436582+franklinnolasco7@users.noreply.github.com";
+        signingkey = "/home/frank/.ssh/id_ed25519.pub";
+      };
+      core = {
+        editor = "vim";
+      };
+      gpg = {
+        format = "ssh";
+        ssh.allowedSignersFile = "/home/frank/.config/git/allowed_signers";
+      };
+      commit = {
+        gpgsign = true;
+      };
       init.defaultBranch = "main";
       fetch.prune = true;
       push.autoSetupRemote = true;
       pull.rebase = false;
     };
   };
+
+  # SSH public key used for commit signature verification.
+  # Replace public key below with key from ~/.ssh/id_ed25519.pub when setting up a new system.
+  home.file.".config/git/allowed_signers".text =
+    "116436582+franklinnolasco7@users.noreply.github.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHtOXqQXYBzKOemkphICYNyUGXOBAMe2HH3bxszTd6R0";
 }
